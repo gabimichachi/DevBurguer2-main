63c747f5579ac6257033276c5f2b57e5a7df4dc3
Gabrielli Michachi <gabrielli.paula@aluno.senai.br>
2026-05-07 15:54:54 -0300

    commitinho basico para nao PERDER TUDO POR CAUSA DA TELA AZUL

---
+++ migrates/produtos_usados.sql
@@ -0,0 +1,18 @@
+INSERT INTO Produtos 
+(PRODUTO, DESCRICAO, DESTAQUE, PRECO, FOTO, DISPONIBILIDADE)
+VALUES
+('X-Burguer', 'Hamburguer com queijo e molho especial', 1, 18.90, 'https://www.sabornamesa.com.br/media/k2/items/cache/bf26253d7b8f171dddb155f84ce1d562_XL.jpg', 1),
+
+('X-Salada', 'Hamburguer com queijo, alface e tomate', 2, 21.50, 'https://www.comidaereceitas.com.br/wp-content/uploads/2025/08/foto-cheese-salada-780x519.jpg', 1),
+
+('X-Bacon', 'Hamburguer com bacon crocante e cheddar', 2, 25.00, 'https://www.sabornamesa.com.br/media/k2/items/cache/5098e75e57e36807c173cb7490b1b0d2_XL.jpg', 2),
+
+('X-Tudo', 'Hamburguer completo com bacon, ovo e salada', 1, 32.90, 'https://alemaolanches.festzap.com.br/_core/_uploads//2022/05/111914052238abagj4gk.jpg', 1),
+
+('Batata Frita', 'Porção de batata frita crocante', 1, 14.00, 'https://jornalsemanario.com.br/wp-content/uploads/batatafrita1.jpg', 2),
+
+('Coca-Cola 2L', 'Refrigerante Coca-Cola 2 litros', 2, 12.00, 'https://img.cdndsgni.com/preview/11077304-m.jpg', true),
+
+('MilkShake Chocolate', 'Milkshake sabor chocolate', 1, 16.50, 'https://northernvirginiamag.com/wp-content/uploads/2022/08/milkshake.jpg', 2),
+
+('Hot Dog Especial', 'Cachorro quente com pure e batata palha', 2, 17.90, 'https://s2.glbimg.com/XQq5qXrTCA2t19Nnlo-sIa-ilH4=/620x455/e.glbimg.com/og/ed/f/original/2019/09/13/cachorro-quente-brasileiro.jpg', 2);
--- model/carrinho.py
+++ model/carrinho.py
@@ -11,20 +11,58 @@ def recuperar_carrinho(usuario:str) -> list:
     ITENS_CARRINHOS.QUANTIDADE, 
     PRODUTOS.PRECO,
     PRODUTOS.FOTO
-FROM CARRINHOS
+    FROM CARRINHOS
 
-INNER JOIN USUARIOS 
+    INNER JOIN USUARIOS 
     ON USUARIOS.USUARIO = CARRINHOS.USUARIO 
 
-INNER JOIN ITENS_CARRINHOS 
+    INNER JOIN ITENS_CARRINHOS 
     ON CARRINHOS.COD_CARRINHO = ITENS_CARRINHOS.COD_CARRINHO
 
-INNER JOIN PRODUTOS 
+    INNER JOIN PRODUTOS 
     ON PRODUTOS.CODIGO = ITENS_CARRINHOS.COD_PRODUTO
 
-WHERE USUARIOS.USUARIO = 'aleatorio';
+    WHERE USUARIOS.USUARIO = 'aleatorio';
                     """)
     resultado = cursor.fetchall()
     conexao.close()
     return resultado
+
+
+def inserir_item(usuario, cod_produto, quantidade):
+    conexao, cursor = conectar()
+
+    
+    cursor.execute("""
+        SELECT COD_CARRINHO
+        FROM Carrinhos
+        WHERE USUARIO = %s
+        AND FINALIZADO = 0
+        LIMIT 1;
+    """, [usuario])
+
+    resultado_carrinho = cursor.fetchone()
+
+    
+    if resultado_carrinho:
+        codigo_carrinho = resultado_carrinho["COD_CARRINHO"]
+
+   
+    else:
+        cursor.execute("""
+            INSERT INTO Carrinhos (USUARIO)
+            VALUES (%s);
+        """, [usuario])
+
+        codigo_carrinho = cursor.lastrowid
+
+    
+    cursor.execute("""
+        INSERT INTO Itens_Carrinhos
+        (COD_CARRINHO, COD_PRODUTO, QUANTIDADE)
+        VALUES (%s, %s, %s);
+    """, [codigo_carrinho, cod_produto, quantidade])
+
+    conexao.commit()
+    conexao.close()
 
